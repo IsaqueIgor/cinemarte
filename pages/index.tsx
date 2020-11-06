e1054b73ea6c52fab69def9cbd3ead71ec9c48c3
Isaque Igor <isaqueigorsilva@gmail.com>
2020-11-05 22:21:43 -0300

    chore: cleaning project and add Typescript

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Cinemarte</title>
+      </Head>   
+      <main>
+        <h1>Cinemarte</h1>
+      </main>
+    </div>
+  )
+}
